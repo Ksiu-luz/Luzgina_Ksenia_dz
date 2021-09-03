e90cafa72ea0fa103537a652b6f2abd0a7ea95d1
Ksenia <luzgina-ksyu@mail.ru>
2021-09-03 22:37:30 +0500

    adding files

---
+++ Luzgina_Ksenia_dz_1.py
@@ -0,0 +1,14 @@
+user_time = int(input('Введите кол-во секунд: '))
+units_of_time = ('дн', 'час', 'мин', 'сек')
+seconds_in_units = (86400, 3600, 60, 1)
+result = []
+
+
+for i in range(len(units_of_time)):
+    if user_time // seconds_in_units[i] != 0:
+        result.append(str(user_time // seconds_in_units[i]))
+        result.append(units_of_time[i])
+        user_time = user_time % seconds_in_units[i]
+
+
+print(' '.join(result))
